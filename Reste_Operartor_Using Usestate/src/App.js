732d2ead10cb28c98929192e16601d0b4a1a6147
kanhereaajinath <aajinathkanhere@gmail.com>
2023-06-28 10:15:21 +0530

    Reset_operator_usig_useState

---
+++ Reste_Operartor_Using Usestate/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+//modifyed example using speart operator 
+const Display=(props)=>{
+  return(
+    <div>
+      counter is {props.counter}
+    </div>
+  )
+}
+const Button=(props)=>{
+  return(
+    <button onClick={props.handleEvent}>{props.text}</button>
+  )
+}
+function App() {
+  //instace of setting individual value of each variable in the object we can set only requried variable 
+  //value and use spreate operator for rest of the variable 
+  const [clicks, setClicks]=useState({left:0,right:0})
+  const inrLeft=()=>{
+    const newClicks={...clicks,left:clicks.left+1}
+    setClicks(newClicks)
+  }
+  const inrRight=()=>{
+    const newClicks={...clicks,right:clicks.right+1}
+    setClicks(newClicks)
+  }
+
+    
+  return (
+    <div>
+      <p>Event handling demo </p>
+     <p><Button handleEvent={inrLeft} text={"Left"}></Button></p> 
+    <p> <Button handleEvent={inrRight} text={"Right"}></Button></p> 
+    <p>
+      Left: <Display counter={clicks.left}></Display>
+      <br></br>
+      Right: <Display counter={clicks.right}/>
+    </p>
+    
+      
+    </div>
+   
+  );
+}
+
+export default App;
