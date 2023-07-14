f413437a68ff579271a47a06e8a5b63d7d3cfca8
kanhereaajinath <aajinathkanhere@gmail.com>
2023-07-14 07:59:44 +0530

    React Add Form Data to Table Component Example

---
+++ React Add Form Data to Table Component Example/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+import Profile from "./Profile";
+function App() {
+  return (
+    <div className="container mt-5">
+      <h2>React Add Form Data to Table Component Example</h2>
+      <Profile />
+    </div>
+  );
+}
+export default App;
